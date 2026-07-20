6ad52e5aee5164326ddf72fa5245bcddb7579b80
avisharma42 <156396607+avisharma42@users.noreply.github.com>
2026-07-20 11:36:06 +0530

    Time: 3 ms (33.42%) | Memory: 25.4 MB (11.58%) - LeetSync

---
+++ 3324-split-the-array/split-the-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPossibleToSplit(vector<int>& nums) {
+        int n = nums.size();
+        if(n%2!=0) return false;
+        unordered_map<int,int> mp;
+        for(int i = 0; i< n; i++){
+          mp[nums[i]]++;
+        }
+        for(auto it : mp){
+            if(it.second>=3) return false;
+        }
+        return  true;
+    }
+};
